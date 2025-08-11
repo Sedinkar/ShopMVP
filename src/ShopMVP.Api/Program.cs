be3216c273cdf531f9a8d3ebf03b5d0988a6624f
Sedinkar <iskanderik1000@gmail.com>
2025-08-11 19:49:45 +0500

    US-02 TSK-06: configure Swagger per environment + enable XML docs for API

--- src/ShopMVP.Api/Program.cs
+++ src/ShopMVP.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,8 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "ShopMVP API", Version = "v1", Description = "Public Web API for ShopMVP (MVP)" });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename), includeControllerXmlComments: true);
+ });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
