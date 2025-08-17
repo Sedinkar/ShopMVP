75a82edb6c7a7f6cfb550a9296a36772eb59bb29
Sedinkar <iskanderik1000@gmail.com>
2025-08-17 19:21:17 +0500

    US-02 TSK-08: enable RFC 7807 ProblemDetails, traceId, status pages mapping, Dev vs Prod behavior

--- src/ShopMVP.Api/Program.cs
+++ src/ShopMVP.Api/Program.cs
@@ -2,19 +2,45 @@ using System.Reflection;
 using System.Text;
 using System.Text.Json;
 
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.OpenApi.Models;
 
 using ShopMVP.Api.Health;
 
 var builder = WebApplication.CreateBuilder(args);
+var env = builder.Environment;
 
-// Add services to the container.
-
+// MVC
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+// Problem Details
+builder.Services.AddProblemDetails(options =>
+ options.CustomizeProblemDetails = (context) =>
+ {
+     var traceId = context.HttpContext.TraceIdentifier;
+     context.ProblemDetails.Extensions["traceId"] = traceId;
+     if (string.IsNullOrEmpty(context.ProblemDetails.Instance))
+     {
+         context.ProblemDetails.Instance = context.HttpContext.Request.Path;
+     }
+     if (env.IsDevelopment() && context.Exception is not null)
+     {
+         context.ProblemDetails.Detail = context.Exception.Message;
+     }
+     if (context.ProblemDetails.Status is null)
+     {
+         context.ProblemDetails.Status = StatusCodes.Status500InternalServerError;
+         if (context.HttpContext.Response.StatusCode == 200)
+         {
+             context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+         }
+     }
+ }
+);
 var hcBuilder = builder.Services.AddHealthChecks();
 hcBuilder.AddCheck(name: "self", () => HealthCheckResult.Healthy(), tags: HealthTags.Live);
 hcBuilder.AddCheck(name: "basic", () => HealthCheckResult.Healthy(), tags: HealthTags.Ready);
@@ -26,20 +52,52 @@ builder.Services.AddSwaggerGen(options =>
 });
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    _ = app.UseDeveloperExceptionPage();
     _ = app.UseSwagger();
     _ = app.UseSwaggerUI();
 }
+else if (!app.Environment.IsDevelopment())
+{
+    _ = app.UseExceptionHandler("/error");
+}
 
-app.UseHttpsRedirection();
+app.Map("/error", async context =>
+{
+    var feature = context.Features.Get<IExceptionHandlerFeature>();
+    var path = context.Features.Get<IExceptionHandlerPathFeature>();
+    var ex = feature?.Error;
+    if (context.Response.StatusCode == 200)
+    {
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+    }
+    context.Response.ContentType = "application/problem+json; charset=utf-8";
+    context.Response.Headers["Cache-Control"] = "no-store, no-cache, max-age=0, must-revalidate";
+    context.Response.Headers["Pragma"] = "no-cache";
+    context.Response.Headers["Expires"] = "0";
 
+    var problem = new ProblemDetails();
+
+    problem.Type = "https://httpstatuses.com/500";
+    problem.Title = "Internal Server Error";
+    problem.Status = StatusCodes.Status500InternalServerError;
+    problem.Instance = path?.Path ?? context.Request.Path;
+    problem.Extensions["traceId"] = context.TraceIdentifier;
+
+    ProblemDetailsContext pdc = new ProblemDetailsContext() { HttpContext = context, ProblemDetails = problem, Exception = ex };
+    var pds = context.RequestServices.GetRequiredService<IProblemDetailsService>();
+    await pds.WriteAsync(pdc);
+});
+
+//app.UseHttpsRedirection();
 app.UseAuthorization();
 static Task WriteResponse(HttpContext context, HealthReport healthReport)
 {
     context.Response.ContentType = "application/json; charset=utf-8";
-    /* Just for practice, already disabled by default AllowCachingResponses = false
+    /* Just for practice, already disabled by default in AllowCachingResponses = false
     context.Response.Headers["Cache-Control"] = "no-store, no-cache, max-age=0, must-revalidate";
     context.Response.Headers["Pragma"] = "no-cache";
     context.Response.Headers["Expires"] = "0";
@@ -99,6 +157,30 @@ app.MapHealthChecks("/health/ready", new HealthCheckOptions
         [HealthStatus.Degraded] = StatusCodes.Status200OK,
         [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
     }
+});
+
+app.UseStatusCodePages(async context =>
+{
+    var code = context.HttpContext.Response.StatusCode;
+    if (code != 404 && code != 405)
+    { return; }
+    context.HttpContext.Response.ContentType = "application/problem+json; charset=utf-8";
+    context.HttpContext.Response.Headers["Cache-Control"] = "no-store, no-cache, max-age=0, must-revalidate";
+    context.HttpContext.Response.Headers["Pragma"] = "no-cache";
+    context.HttpContext.Response.Headers["Expires"] = "0";
+
+    var path = context.HttpContext.Request.Path.Value;
+    var problem = new ProblemDetails();
+
+    problem.Type = $"https://httpstatuses.com/{code}";
+    problem.Title = (code == StatusCodes.Status404NotFound) ? "Not Found" : "Method Not Allowed";
+    problem.Status = code;
+    problem.Instance = path;
+    problem.Extensions["traceId"] = context.HttpContext.TraceIdentifier;
+
+    ProblemDetailsContext pdc = new ProblemDetailsContext() { HttpContext=context.HttpContext, ProblemDetails = problem, Exception = null };
+    var pds = context.HttpContext.RequestServices.GetRequiredService<IProblemDetailsService>();
+    await pds.WriteAsync(pdc);
 });
 
 app.MapControllers();
