668578c687c34443db341dfcb61e5fec20620ece
Sedinkar <iskanderik1000@gmail.com>
2025-08-21 21:49:46 +0500

    US-03 TSK-11: register AppDbContext with Dev connection string (UseSqlServer, retry police, logging)

--- src/ShopMVP.Infrastructure/DependencyInjection.cs
+++ src/ShopMVP.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,19 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using ShopMVP.Infrastructure.Persistence;
 
 namespace ShopMVP.Infrastructure.DependencyInjection
+
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddPersistence(this IServiceCollection services, string connectionString)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(connectionString, sql =>
+            sql.EnableRetryOnFailure(maxRetryCount: 5, maxRetryDelay: TimeSpan.ParseExact("00:00:10","c",null), errorNumbersToAdd: null)));
+            
             return services;
         }
     }
